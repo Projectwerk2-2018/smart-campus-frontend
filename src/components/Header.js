0e543edb90c1979cd8fde44786bfd56633a577c4
Gulumulu <quirschfeld@gmail.com>
2018-03-20 18:07:27 +0100

    added floor plan with :onHover hihglights of rooms

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
         return(
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a class="navbar-brand" href="#">
-                    <img src="../img/logo.png" width="30" height="30" class="d-inline-block align-top" alt=""></img>
+                    <img src={require("../img/logo.png")} width="30" height="30" class="d-inline-block align-top" alt=""></img>
                     SMART CAMPUS
                 </a>
                 <div class="container-fluid">
--- src/views/Building.js
+++ src/views/Building.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import ImageMapper from 'react-image-mapper';
 
 class Building extends Component {
-	  render() {
+
+	constructor(props) {
+		super(props); 
+
+		this.state ={
+			MAP : {
+				name: "my-map",
+				areas: [
+				  { shape: "rect", coords: [1249,209,1492,513] },
+				  { shape: "rect", coords: [126,250,264,513] },
+				  { shape: "rect", coords: [271,250,450,513] },
+				  { shape: "rect", coords: [455,250,615,513] },
+				  { shape: "rect", coords: [620,250,780,513] }
+				]
+			}
+		}
+	}
+
+	//<img src={require("../img/floor.svg")} className="img-fluid floor" alt="Responsive image"></img>
+
+	render() {
 	      return(
-		    <div className="App">
-                Hello! This is building.
-            </div>
+			<div class="row">
+				<div class="col-9">
+					<ImageMapper src={require("../img/floor_plan.svg")} map={this.state.MAP} width={1500} fillColor={"rgba(141, 128, 229, 0.6)"}/>
+				</div>
+				<div class="col-3">
+				
+				</div>
+			</div>
 	      );
 	  }
 }
