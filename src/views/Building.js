a712d51a09d96565abe634641afa7954a508006f
Gulumulu <quirschfeld@gmail.com>
2018-03-21 18:19:44 +0100

    page refreshes after window resize

--- src/views/Building.js
+++ src/views/Building.js
@@ -30,7 +30,7 @@ class Building extends Component {
 				areas: [
 				  { shape: "rect", coords: [width*0.583, width*0.097, width*0.696, width*0.239] },
 				  { shape: "rect", coords: [width*0.059, width*0.116, width*0.123, width*0.239] },
-				  { shape: "rect", coords: [width*0.126, width*0.116, width*0.21, width*0.239] },
+				  { shape: "rect", coords: [width*0.126, width*0.116, width*0.210, width*0.239] },
 				  { shape: "rect", coords: [width*0.213, width*0.116, width*0.287, width*0.239] },
 				  { shape: "rect", coords: [width*0.289, width*0.116, width*0.364, width*0.239] }
 				]
@@ -42,10 +42,13 @@ class Building extends Component {
 	componentWillMount() {
 			this.updateDimensions();
 	}
+
+	shouldComponentUpdate() {
+		window.location.reload()
+	}
 	
 	componentDidMount() {
 			window.addEventListener("resize", this.updateDimensions);
-			this.forceUpdate();
 	}
 	
 	componentWillUnmount() {
@@ -54,12 +57,9 @@ class Building extends Component {
 
 	//<img src={require("../img/floor.svg")} className="img-fluid floor" alt="Responsive image"></img>
 	//<ImageMapper src={require("../img/floor_plan.svg")} map={MAP} width={this.state.width} fillColor={"rgba(141, 128, 229, 0.6)"}/>
-	//[1249, 209, 1492, 513]
 
 	render() {
-		console.log(this.state.width)
-
-		var style = {position: 'absolute', top: 600, left: 100}
+		var style = {position: 'absolute', top: 300, left: 150}
 
 	    return (
 			<div class="row">
